293d21e051ddc94f283cc1939a194a0e0ade1fae
Tyler Fisher <tfisher21@gmail.com>
2018-11-18 21:28:51 -0600

    Create request to api/users and list all users

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="users">
     <h1>{{ message }}</h1>
+    <div v-for="user in users">
+      <h3>{{ user.first_name }} {{ user.last_name }}</h3>
+      <p>{{ user.email }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +12,26 @@
 </style>
 
 <script>
+var axios = require('axios');
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Users",
+      users: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios({
+      method: 'get',
+      url: 'http://localhost:3000/api/users',
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozLCJleHAiOjE1NDI2NjUzODJ9.WkTIpp7F_7kvjN8OLYbIqUA4ijLAHC5U1BF1AWtS9OE'
+      }
+    }).then(function(response) {
+      this.users = response.data;
+    }.bind(this));
+  },
   methods: {},
   computed: {}
 };
