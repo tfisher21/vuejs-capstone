8236a802bac306b2eb333f7fb6f82f04c054f741
Tyler Fisher <tfisher21@gmail.com>
2018-11-24 10:58:22 -0500

    Add the ability to choose whehter you want to see all posts or just your cohorts posts. Default should be cohort posts.

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -2,38 +2,49 @@
   <div class="users">
     <h1>{{ message }}</h1>
     <div class="row justify-content-center">
-      <div class="col-6">
-
-        <div class="card card-body">
-          <button class="btn btn-lg btn-vuejs" data-toggle="collapse" href="#newPost" role="button" aria-expanded="false" aria-controls="newPost" v-on:click="showCreatePost = !showCreatePost">
-            <div v-if="showCreatePost">
-              Close
-            </div>
-            <div v-else>
-              Create New Post
-            </div>
-          </button>
-          <div class="collapse mt-2" id="newPost">
-            <form v-on:submit.prevent="publish()">
-              <input class="form-control form-control-lg my-1" type="text" v-model="newTitle" placeholder="Title">
-              <textarea class="form-control my-1" cols="30" rows="10" v-model="newContent" placeholder="Start writing something..."></textarea>
-              <div class="row justify-content-end">
-                <ul class="list-unstyled mt-2">
-                  <li class="text-danger" v-for="error in errors">{{ error }}</li>
-                </ul>
-                <div class="col-4 text-right">
-                  <input type="button" class="text-right btn btn-secondary mr-1" value="Clear" v-on:click="clearPost()">
-                  <input type="submit" class="text-right btn btn-vuejs" value="Publish!">
-                </div>
+      <div class="col-6"> 
+        <div class="card">
+          <div class="card-header">
+            <ul class="nav nav-pills nav-justified">
+              <li class="nav-item">
+                <a class="nav-link" href="#" v-on:click="showCohortPosts()">Cohort Posts</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" v-on:click="showAllPosts()">All Posts</a>
+              </li>
+            </ul>
+          </div>
+          <div class="card-body">
+            <button class="btn btn-lg btn-vuejs w-100" data-toggle="collapse" href="#newPost" role="button" aria-expanded="false" aria-controls="newPost" v-on:click="showCreatePost = !showCreatePost">
+              <div v-if="showCreatePost">
+                Close
               </div>
-            </form>
+              <div v-else>
+                Create New Post
+              </div>
+            </button>
+            <div class="collapse mt-2" id="newPost">
+              <form v-on:submit.prevent="publish()">
+                <input class="form-control form-control-lg my-1" type="text" v-model="newTitle" placeholder="Title">
+                <textarea class="form-control my-1" cols="30" rows="10" v-model="newContent" placeholder="Start writing something..."></textarea>
+                <div class="row justify-content-end">
+                  <ul class="list-unstyled mt-2">
+                    <li class="text-danger" v-for="error in errors">{{ error }}</li>
+                  </ul>
+                  <div class="col-4 text-right">
+                    <input type="button" class="text-right btn btn-secondary mr-1" value="Clear" v-on:click="clearPost()">
+                    <input type="submit" class="text-right btn btn-vuejs" value="Publish!">
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div v-for="post in posts" class="row justify-content-center">
       <div class="col-6">
-        <div class="card h-100">
+        <div class="card">
           <div class="card-body">
             <h3 class="card-title">{{ post.title }}</h3>
             <p class="card-subtitle text-muted">{{ post.author }}</p>
@@ -68,12 +79,12 @@ export default {
     };
   },
   created: function() {
-    axios({
-      method: 'get',
-      url: 'http://localhost:3000/api/posts'
-    }).then(function(response) {
+    var params = {
+      sort_by_cohort: true
+    };
+    axios.get("http://localhost:3000/api/posts", {params}).then(response => {
       this.posts = response.data;
-    }.bind(this));
+    });
   },
   methods: {
     clearPost: function() {
@@ -94,6 +105,20 @@ export default {
         this.newContent = "";
       }).catch(error => {
         this.errors = error.response.data.errors;
+      });
+    },
+    showAllPosts: function() {
+      axios.get("http://localhost:3000/api/posts").then(response => {
+        this.posts = response.data;
+      });
+    },
+    showCohortPosts: function() {
+      axios.get("http://localhost:3000/api/posts", {
+        params: {
+          sort_by_cohort: true
+        }
+      }).then(response => {
+        this.posts = response.data;
       });
     }
   },
