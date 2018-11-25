26827698d027f7f0d96123c6c54ffe47f605657d
Tyler Fisher <tfisher21@gmail.com>
2018-11-25 09:14:59 -0600

    Add jumbotron to home view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
     <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-      <ul class="navbar-nav mx-auto">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/signup">Signup</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/login">Login</router-link>
-        </li>
-        <li class="nav-item">
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/logout">Logout</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/users">Users</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/posts">Posts</router-link>
-        </li>
-      </ul>
+      <div class="container-fluid">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/users">Users</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/posts">Posts</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/logout">Logout</router-link>
+          </li>
+        </ul>
+      </div>
     </nav>
       <div class="container-fluid mt-4">
         <router-view/>
@@ -35,7 +39,7 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <router-link class="btn btn-vuejs mx-1" to="/signup">Signup</router-link>
-    <router-link class="btn btn-vuejs mx-1" to="/login">Login</router-link>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>{{ message }}</h1>
+        <p class="lead">We want to help you communicate with your cohort and the graduates of the Actualize Bootcamp.</p>
+        <hr class="my-4">
+        <div id="homeLogin">
+          <p>Please Login or Signup to get started!</p>
+          <router-link class="btn btn-vuejs mx-1" to="/login">Login</router-link>
+          <router-link class="btn btn-vuejs mx-1" to="/signup">Signup</router-link>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <style>
+#homeLogin {
+  text-align: center;
+}
+
 .btn-vuejs {
   background-color: #42b983;
   color: white;
