bebd0b143647b0f8ffe1bc41424623a78582f425
Tyler Fisher <tfisher21@gmail.com>
2018-12-23 13:56:36 -0600

    Add a link to the Users Index view to the navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav
+      id="nav"
+      class="navbar navbar-expand-lg navbar-light bg-light sticky-top"
+    >
       <div class="container-fluid">
         <ul class="navbar-nav">
           <li class="nav-item">
@@ -9,6 +12,9 @@
         </ul>
         <div v-if="jwt">
           <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/users">Users</router-link>
+            </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/logout">Logout</router-link>
             </li>
@@ -26,15 +32,13 @@
         </div>
       </div>
     </nav>
-      <div class="container-fluid mt-4">
-        <router-view/>
-      </div>
+    <div class="container-fluid mt-4"><router-view /></div>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /*text-align: center;*/
@@ -52,11 +56,10 @@
 </style>
 
 <script>
-
 export default {
   data: function() {
     return {
-      jwt: false,
+      jwt: false
     };
   },
   created: function() {
